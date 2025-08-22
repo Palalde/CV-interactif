97f2278e79a0a7584dc80d6a43e3c45e0d821f84
Palalde <Palessandrini13@gmail.com>
2025-08-23 01:52:50 +0200

    vercel compatible ?

--- js/utility/theme-toggle.js
+++ js/utility/theme-toggle.js
@@ -2,74 +2,74 @@ function updateNavIcons(isLight) {
   // LOGO
   document.querySelectorAll('.cv-logo').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/cv-noir.png'
-      : '../img/nav-icon/darkmode/cv-blanc.png';
+  ? '/img/nav-icon/lightmode/cv-noir.png'
+  : '/img/nav-icon/darkmode/cv-blanc.png';
   });
   // SEARCH BAR
    document.querySelectorAll('.search-icon').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/loupe-beige.png'
-      : '../img/nav-icon/darkmode/loupe-bleu.png';
+  ? '/img/nav-icon/lightmode/loupe-beige.png'
+  : '/img/nav-icon/darkmode/loupe-bleu.png';
   });
   // NAV ICON
   // Loupe
   document.querySelectorAll('.icon-loupe.main').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/loupe-noir.png'
-      : '../img/nav-icon/darkmode/loupe-blanc.png';
+  ? '/img/nav-icon/lightmode/loupe-noir.png'
+  : '/img/nav-icon/darkmode/loupe-blanc.png';
   });
   document.querySelectorAll('.icon-loupe.second').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/loupe-beige.png'
-      : '../img/nav-icon/darkmode/loupe-bleu.png';
+  ? '/img/nav-icon/lightmode/loupe-beige.png'
+  : '/img/nav-icon/darkmode/loupe-bleu.png';
   });
   // Contact
   document.querySelectorAll('.icon-contact.main').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/contact-noir.png'
-      : '../img/nav-icon/darkmode/contact-blanc.png';
+  ? '/img/nav-icon/lightmode/contact-noir.png'
+  : '/img/nav-icon/darkmode/contact-blanc.png';
   });
   document.querySelectorAll('.icon-contact.second').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/contact-beige.png'
-      : '../img/nav-icon/darkmode/contact-bleu.png';
+  ? '/img/nav-icon/lightmode/contact-beige.png'
+  : '/img/nav-icon/darkmode/contact-bleu.png';
   });
   // PDF
   document.querySelectorAll('.icon-pdf.main').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/pdf-noir.png'
-      : '../img/nav-icon/darkmode/pdf-blanc.png';
+  ? '/img/nav-icon/lightmode/pdf-noir.png'
+  : '/img/nav-icon/darkmode/pdf-blanc.png';
   });
   document.querySelectorAll('.icon-pdf.second').forEach(img => {
     img.src = isLight
-      ? '../img/nav-icon/lightmode/pdf-beige.png'
-      : '../img/nav-icon/darkmode/pdf-bleu.png';
+  ? '/img/nav-icon/lightmode/pdf-beige.png'
+  : '/img/nav-icon/darkmode/pdf-bleu.png';
   });
 
   // CONTACT SIDE ICONS
   // Calendrier
   document.querySelectorAll('.contact-icon[alt="Âge"]').forEach(img => {
     img.src = isLight
-      ? '../img/contact/lightmode/calendrier-noir.png'
-      : '../img/contact/darkmode/calendrier-blanc.png';
+  ? '/img/contact/lightmode/calendrier-noir.png'
+  : '/img/contact/darkmode/calendrier-blanc.png';
   });
   // Ping
   document.querySelectorAll('.contact-icon[alt="Localisation"]').forEach(img => {
     img.src = isLight
-      ? '../img/contact/lightmode/ping-noir.png'
-      : '../img/contact/darkmode/ping-blanc.png';
+  ? '/img/contact/lightmode/ping-noir.png'
+  : '/img/contact/darkmode/ping-blanc.png';
   });
   // LinkedIn
   document.querySelectorAll('.contact-icon.social-icon[alt="LinkedIn"]').forEach(img => {
     img.src = isLight
-      ? '../img/contact/lightmode/linkedin-noir.png'
-      : '../img/contact/darkmode/linkedin-blanc.png';
+  ? '/img/contact/lightmode/linkedin-noir.png'
+  : '/img/contact/darkmode/linkedin-blanc.png';
   });
   // GitHub
   document.querySelectorAll('.contact-icon.social-icon[alt="GitHub"]').forEach(img => {
     img.src = isLight
-      ? '../img/contact/lightmode/github-noir.png'
-      : '../img/contact/darkmode/github-blanc.png';
+  ? '/img/contact/lightmode/github-noir.png'
+  : '/img/contact/darkmode/github-blanc.png';
   });
 }
 
